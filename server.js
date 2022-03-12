d90d5f813de621c56cb49c7b192fb928b712dce3
koushik-murali <m.koushik1995@gmail.com>
2022-03-13 00:46:30 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const article = require('./articles')
+const images = require('./pictures')
+const dotenv = require('dotenv');
+const Mongoose = require('mongoose');
+dotenv.config();
+Mongoose.connect(process.env.CONSTRING, {useNewUrlParser: true, useUnifiedTopology: true,})
+
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res) =>{
+    try{
+        const articles = await article.find()
+        const websiteImages = await images.find()
+        res.render('home', { Pictures: websiteImages, articles: articles})
+    } catch(e)
+    {
+        console.log(e)
+    }
+    
+} 
+)
+app.listen(process.env.PORT || 3000);
